f1f8564a6bd839ec37b2d330686145d9ef37bfde
nofar <nofar.alfasi@s.afeka.ac.il>
2019-04-24 17:10:03 +0300

    Update BinarySearchImpl.java

--- SortAlgorithms/src/main/java/com/sortAlgorithms/spring/BinarySearchImpl.java
+++ SortAlgorithms/src/main/java/com/sortAlgorithms/spring/BinarySearchImpl.java
@@ -1,4 +1,4 @@
-package com.myfirst.spring.fromwebsite.from28minutes;
+package com.sortAlgorithms.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
