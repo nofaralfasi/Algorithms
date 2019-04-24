235738020012b1e66c36b1b3fcd1308ea204b174
nofar <nofar.alfasi@s.afeka.ac.il>
2019-04-24 17:10:40 +0300

    Update BubbleSortAlgorithm.java

--- SortAlgorithms/src/main/java/com/sortAlgorithms/spring/BubbleSortAlgorithm.java
+++ SortAlgorithms/src/main/java/com/sortAlgorithms/spring/BubbleSortAlgorithm.java
@@ -1,4 +1,4 @@
-package com.myfirst.spring.fromwebsite.from28minutes;
+package com.sortAlgorithms.spring;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
