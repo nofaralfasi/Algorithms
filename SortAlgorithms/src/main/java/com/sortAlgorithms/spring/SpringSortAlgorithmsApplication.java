7739d04604abf5d307c88ef85a061c92682ab20b
nofar <nofar.alfasi@s.afeka.ac.il>
2019-04-24 17:10:45 +0300

    Update SpringSortAlgorithmsApplication.java

--- SortAlgorithms/src/main/java/com/sortAlgorithms/spring/SpringSortAlgorithmsApplication.java
+++ SortAlgorithms/src/main/java/com/sortAlgorithms/spring/SpringSortAlgorithmsApplication.java
@@ -1,4 +1,4 @@
-package com.myfirst.spring.fromwebsite;
+package com.sortAlgorithms.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
