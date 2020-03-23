f3b033d00335ce28a2483da3bb8585a4e6e24a7a
nofar <nofar.alfasi@s.afeka.ac.il>
2020-03-23 20:03:37 +0200

    Update converts.c

--- Quizes/converts.c
+++ Quizes/converts.c
@@ -1,51 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fromIntToChar(int num, char* str)
+char* fromIntToChar(int num)
 {
-   int i, counter, tmp;
-   tmp=num;
-   counter=1;
-   while(tmp/10 != 0)
+   char* str;
+   int i, neg=0, counter=1, tmp=num;
+
+   if(num<0)
+   {
+      neg = 1;
+      counter++;
+      num = -1*num;
+   }
+
+     while(tmp/10 != 0)
    {
       counter++;
       tmp/=10;
    }
 
-   if(num<0)
-   {
-      counter++;
-   }
-  
   str=(char*)malloc((counter+1)*sizeof(char));
-   if (str == NULL) {
-      free(str);
-      printf("Error allocating memory!\n");
-      return;
-      }
 
-  str[counter]='\0';
-  
+  str[counter]='\0';  
   tmp=num;
+  
   for(i=1; i<=counter; i++)
   {
-     str[counter-i] = tmp%10;
-     tmp/10;
+     str[counter-i] =tmp%10+ '0';
+     tmp/=10;
   }
 
-  if(num<0)
+  if(neg)
   {
      str[0]='-';
   }
 
+   return str;
 }
 
 int main()
 {
-   int num=7392;
+   int num=-7392;
    char* str;
 
-   fromIntToChar(num, str);
+   str=fromIntToChar(num);
 
 	printf("Num as int: %d\n", num);
    printf("Num as string: %s\n", str);
