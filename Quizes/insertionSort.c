0845f0e5e6b27f0c9a2c069500fb2f90eb120f7f
nofar <nofar.alfasi@s.afeka.ac.il>
2020-03-23 20:14:47 +0200

    Update insertionSort.c

--- Quizes/insertionSort.c
+++ Quizes/insertionSort.c
@@ -6,16 +6,21 @@ void InsertionSort (int arr[], int size)
 {
   int i, key, j;
   for (i = 1; i < size; i++)
+  {
+    key = arr[i];
+    j = i - 1;
+
+    /* Move elements of arr[0..i-1], that are 
+    greater than key, to one position ahead 
+    of their current position */
+    while (j >= 0 && arr[j] > key)
     {
-      key = arr[i];
-      j = i - 1;
-      while (j >= 0 && arr[j] > key)
-	{
-	  arr[j + 1] = arr[j];
-	  j = j - 1;
-	}
-      arr[j + 1] = key;
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
+
+    arr[j + 1] = key;
+  }
 }
 
 // A utility function to print an array of size size 
@@ -23,7 +28,7 @@ void printArray (int arr[], int size)
 {
   int i;
   for (i = 0; i < size; i++)
-    printf ("%d ", arr[i]);
+  printf ("%d ", arr[i]);
   printf ("\n");
 }
 
